c3d042738ef9c65dd2b75369c990f0a70256478e
Sarah Shuey <sarah.shuey@me.com>
2017-09-27 11:30:42 -0500

    inital commit

---
+++ src/actionTypes/actions.js
@@ -0,0 +1,7 @@
+export const DETAILS = 'DETAILS';
+// Export four action types:  FILTER_WORLDS, FILTER_FILMS, FILTER_STARSHIPS, RESET
+// Write your code below.
+export const FILTER_WORLDS = 'FILTER_WORLDS';
+export const FILTER_FILMS = 'FILTER_FILMS';
+export const FILTER_STARSHIPS = 'FILTER_STARSHIPS';
+export const RESET = 'RESET';
